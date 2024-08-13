a1fa39d299f3c093b7f044d374bff9b4208184dd
Alonso <alonso.rbass@gmail.com>
2024-08-13 23:42:38 +0200

    update: organize architecture folders and files + menu

---
+++ public/assets/js/menu.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const iconElement = document.querySelector("nav");
+    const ulElement = document.querySelector("ul");
+  
+    iconElement.addEventListener("click", function () {
+
+      ulElement.classList.toggle("open");
+    });
+  });
+
+
