1409463b3d64af4833911849003b86ba472b1533
Alonso <alonso.rbass@gmail.com>
2024-08-18 23:55:30 +0200

    update: test code

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,45 +1,56 @@
-var player = document.querySelector('canvas')
-var content = player.getContext('2d')
-var img = new Image();
-img.src = "/public/assets/img/aviao"
-img.addEventListener('load', function () {
-    loop();
-}, false);
+// var cnv = document.querySelector('canvas');
+// cnv.width = cnv.clientWidth;
+// cnv.height = cnv.clientHeight;
+// var ctx = cnv.getContext('2d');
 
-var obj = {
-    x: 100,
-    y: 100,
-    width: 100,
-    height: 100,
-    rotation: 0,
-    img: img,
-    centerX: function () {
-        return this.x + this.width / 2;
-    },
-    centerY: function () {
-        return this.y + this.height / 2;
-    }
-}
+// // Obtendo a imagem diretamente do HTML
+// var img = document.querySelector('.player');
 
-function loop() {
-    requestAnimationFrame(loop);
-    update();
-    render();
-}
+// var obj = {
+//     x: 100,  // Ajuste as coordenadas de acordo com o tamanho do canvas
+//     y: 100, // Ajuste as coordenadas de acordo com o tamanho do canvas
+//     width: 576,
+//     height: 576,
+//     rotation: 0,
+//     img: img,
+//     centerX: function () {
+//         return this.x + this.width / 2;
+//     },
+//     centerY: function () {
+//         return this.y + this.height / 2;
+//     }
+// };
 
-function update() {
-    // Aqui você pode adicionar lógica para mover ou girar o objeto
-}
+// var mouse = {};
+// cnv.addEventListener('mousemove', function (e) {
+//     mouse.x = e.clientX - cnv.offsetLeft
+//     mouse.y = e.clientY - cnv.offsetTop
+// }, false);
 
-function render() {
-    content.clearRect(0, 0, player.width, player.height);
-    content.save();
-    content.translate(obj.centerX(), obj.centerY());
-    content.rotate(obj.rotation); // Mantenha 0 ou ajuste conforme necessário
-    content.drawImage(
-        obj.img,
-        0, 0, obj.width, obj.height,
-        -obj.width / 2, -obj.height / 2, obj.width, obj.height
-    );
-    content.restore();
-}
+// function loop() {
+//     requestAnimationFrame(loop);
+//     update();
+//     render();
+// }
+
+// function update() {
+//    console.log(mouse.x)
+// }
+
+// function render() {
+//     ctx.clearRect(0, 0, cnv.width, cnv.height);
+//     ctx.save();
+//     ctx.translate(obj.centerX(), obj.centerY());
+//     ctx.rotate(obj.rotation);
+//     ctx.drawImage(
+//         obj.img,
+//         0, 0, obj.width, obj.height,
+//         -obj.width / 2, -obj.height / 2, obj.width, obj.height
+//     );
+//     ctx.restore();
+// }
+
+// loop(); // Inicie o loop
+
+
+
