9e91697f5cc445c9dfafea03f03279bb0144758c
Alonso <alonso.rbass@gmail.com>
2024-11-04 15:24:45 +0100

    feat: clean code and commenting functions

--- app/public/assets/js/obstacle.js
+++ app/public/assets/js/obstacle.js
@@ -1,5 +1,7 @@
 window.onload = function(){
   const gameBoard = document.querySelector('.game-board')
+
+  // Make sure the gameBoard is selected correctly
   if(!gameBoard){
     console.error('Element .game-board not found!');
     return;
@@ -8,21 +10,26 @@ window.onload = function(){
   const gameBoardHeight = gameBoard.clientHeight;
   const gameBoardWidth = gameBoard.clientWidth;
 
+  // Function to generate an obstacle
   function createObstacle(){
     const obstacle = document.createElement('div');
     obstacle.classList.add('obstacle');
 
+    // Randomly position on the X axis
     const randomX = Math.floor(Math.random() * (gameBoardWidth - 50))
     obstacle.style.left = `${randomX}px`;
     obstacle.style.top = `0px`;
-
     gameBoard.appendChild(obstacle);
+
+    // Set a random fall speed
     const speed = Math.random() * 3 + 2;
 
+    // Update the obstacle position every frame
     function fall(){
       const currentY = parseFloat(obstacle.style.top);
       obstacle.style.top = `${currentY + speed}px`;
 
+      // Update the obstacle position every frame
       if (currentY + obstacle.clientHeight >= gameBoardHeight - 2){
         obstacle.remove();
       } else {
@@ -32,12 +39,14 @@ window.onload = function(){
     fall();
   }
 
+  // Function to generate obstacles at random intervals
   function generateObstacles() {
     setInterval(() => {
       createObstacle();
     }, Math.random() * 2000 + 1000)
   }
 
+  // Start generating obstacles
   generateObstacles();
 
 }
