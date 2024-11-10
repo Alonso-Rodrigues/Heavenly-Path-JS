a100da8c071c12b2040a77ed31e40d90fa9bde89
Alonso <alonso.rbass@gmail.com>
2024-11-10 22:58:48 +0100

    feat: update lifeBar

--- app/public/assets/js/damages.js
+++ app/public/assets/js/damages.js
@@ -1,54 +1,56 @@
-let life = 5; 
+document.addEventListener('DOMContentLoaded', () => {
+  let life = 5;
 
-const lifeScore = document.querySelector('.score');
-const lifeCounter = document.querySelector('.lifeCounter');
-const lifeFace = document.querySelector('.lifeFace');
+  const lifeScore = document.querySelector('.score');
+  const lifeCounter = document.querySelector('.lifeCounter');
+  const lifeFace = document.querySelector('.lifeFace');
 
-function takeDamage() {
-  if (life > 0) {
-    life--; 
-    updateLifeImages();
+  function takeDamage() {
+    if (life > 0) {
+      life--;
+      updateLifeImages();
+    }
   }
-}
 
-function updateLifeImages() {
-  switch (life) {
-    case 5:
-      lifeScore.style.backgroundImage = "url('../public/assets/img/lifeScore/life1x.png')";
-      lifeCounter.style.backgroundImage = "url('../public/assets/img/heartScore/heart1.png')";
-      lifeFace.style.backgroundImage = "url('../public/assets/img/game/health.png')";
-      break;
-    case 4:
-      lifeScore.style.backgroundImage = "url('../public/assets/img/lifeScore/life4x.png')";
-      lifeCounter.style.backgroundImage = "url('../public/assets/img/heartScore/heart2.png')";
-      lifeFace.style.backgroundImage = "url('../public/assets/img/game/health.png')";
-      break;
-    case 3:
-      lifeScore.style.backgroundImage = "url('../public/assets/img/lifeScore/life3x.png')";
-      lifeCounter.style.backgroundImage = "url('../public/assets/img/heartScore/heart3.png')";
-      lifeFace.style.backgroundImage = "url('../public/assets/img/game/health.png')";
-      break;
-    case 2:
-      lifeScore.style.backgroundImage = "url('../public/assets/img/lifeScore/life2x.png')";
-      lifeCounter.style.backgroundImage = "url('../public/assets/img/heartScore/heart4.png')";
-      lifeFace.style.backgroundImage = "url('../public/assets/img/game/babe.png')";
-      break;
-    case 1:
-      lifeScore.style.backgroundImage = "url('../public/assets/img/lifeScore/life1x.png')";
-      lifeCounter.style.backgroundImage = "url('../public/assets/img/heartScore/heart5.png')";
-      lifeFace.style.backgroundImage = "url('../public/assets/img/game/babe.png')";
-      break;
-    default:
-      lifeScore.style.backgroundImage = "url('../public/assets/img/lifeScore/gameOver.png')";
-      lifeCounter.style.backgroundImage = "url('../public/assets/img/heartScore/heart6.png')";
-      lifeFace.style.backgroundImage = "url('../public/assets/img/game/death.gif')";
-      break;
+  function updateLifeImages() {
+    switch (life) {
+      case 5:
+        lifeScore.src = "../public/assets/img/lifeScore/life5x.png";
+        lifeCounter.src = "../public/assets/img/heartScore/heart1.png";
+        lifeFace.src = "../public/assets/img/game/health.png";
+        break;
+      case 4:
+        lifeScore.src = "../public/assets/img/lifeScore/life4x.png";
+        lifeCounter.src = "../public/assets/img/heartScore/heart2.png";
+        lifeFace.src = "../public/assets/img/game/health.png";
+        break;
+      case 3:
+        lifeScore.src = "../public/assets/img/lifeScore/life3x.png";
+        lifeCounter.src = "../public/assets/img/heartScore/heart3.png";
+        lifeFace.src = "../public/assets/img/game/health.png";
+        break;
+      case 2:
+        lifeScore.src = "../public/assets/img/lifeScore/life2x.png";
+        lifeCounter.src = "../public/assets/img/heartScore/heart4.png";
+        lifeFace.src = "../public/assets/img/game/babe.png";
+        break;
+      case 1:
+        lifeScore.src = "../public/assets/img/lifeScore/life1x.png";
+        lifeCounter.src = "../public/assets/img/heartScore/heart5.png";
+        lifeFace.src = "../public/assets/img/game/babe.png";
+        break;
+      default:
+        lifeScore.src = "../public/assets/img/lifeScore/gameOver.png";
+        lifeCounter.src = "../public/assets/img/heartScore/heart6.png";
+        lifeFace.src = "../public/assets/img/game/death.gif";
+        break;
+    }
   }
-}
 
-// Exemplo de evento para simular dano
-document.addEventListener('keydown', (event) => {
-  if (event.key === ' ') { 
-    takeDamage();
-  }
+  document.addEventListener('keydown', (event) => {
+    if (event.key === ' ') {
+      takeDamage();
+    }
+  });
 });
+``
